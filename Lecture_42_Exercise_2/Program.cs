83e846953f70a976b102b633846149a25561328c
The Polka Warlord <The Polka Warlord>
2017-04-26 14:57:14 -0500

    Add project files.

---
+++ Lecture_42_Exercise_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_42_Exercise_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
