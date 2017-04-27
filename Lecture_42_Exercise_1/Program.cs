f0d2bd6ce7de684e7f5a6d267ea02870d0f8ba1f
Ryan <rjarmon@gmail.com>
2017-04-27 00:58:01 -0500

    Initial

--- Lecture_42_Exercise_1/Program.cs
+++ Lecture_42_Exercise_1/Program.cs
@@ -10,6 +10,16 @@ namespace Lecture_42_Exercise_1
 	{
 		static void Main(string[] args)
 		{
+			int number = 7;
+
+			if (number > 1 && number < 10)
+			{
+				Console.WriteLine("Valid");
+			}
+			else
+			{
+				Console.WriteLine("Invalid");
+			}
 		}
 	}
 }
